a7a6310a9cb40d8664ed9924a82b064b12daa342
Mahmudul Hasan <shuvonovo@gmail.com>
2022-03-05 15:31:38 +0600

    created Header and card UI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bengalLPG</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+    header{
+        background-color: red;
+        height:50px;
+        
+    }
+    .header-right{
+        color:white;
+        padding-top:15px;
+        padding-right:25px;
+        float:right;
+    }
+    .card{
+         margin-left:50px;
+         margin-right:50px;
+        box-shadow: 0 4px 8px 0 rgb(255,255,255);
+      
+        height:100%;
+        
+    }
+    nav{
+        padding-top:30px;
+        float:right;
+        text-decoration: none;
+
+    }
+    ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  
+}
+
+li {
+    
+  display: inline;
+  /* text-decoration: none; */
+  margin-right: 20px;
+  color:black;
+  text-decoration: none;
+
+}
+    </style>
+</head>
+<body>
+    <div class="card">
+<header>
+    <div></div>
+  <div class="header-right">CONTACT US</div>
+</header>
+<nav>
+<ul>
+  <li><a href="">HOME</a></li>
+  <li><a href="">ABOUT US</a></li>
+  <li><a href="">LPG PRODUCTS</a></li>
+  <li><a href="">SERVICES</a></li>
+  <li><a href="">DISTRIBUTORS</a></li>
+  <li><a href="">GALLERY</a></li>
+  <li><a href="">CONTACT US</a></li>
+</ul>
+</nav>
+</div>  
+</body>
+</html>
